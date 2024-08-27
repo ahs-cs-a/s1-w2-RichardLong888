f121524eb8a3437224bfc2a91871250079f185b9
RichardLong888 <matthewtan2008@gmail.com>
2024-08-27 05:41:34 +0000

    2

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,12 +1,6 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
         BodyOfWater b = new BodyOfWater("River", "90 meters", 20, false);
-        
-        b.setName("River");
-        b.setLargestDiameter("90 meters");
-        b.setAvgDepth(20);
-        b.setIsSaltWater(false);
-    
         System.out.println(b.getName());
         System.out.println(b.getLargestDiameter());
         System.out.println(b.getAvgDepth());
@@ -14,8 +8,6 @@ public class BodyOfWaterMain {
 
 
         BodyOfWater b1 = new BodyOfWater("Lagoon", "50 miles");
-        b1.setName("Lagoon");
-        b1.setLargestDiameter("50 miles");
         System.out.println(b1.getName());
         System.out.println(b1.getLargestDiameter());
         b1.setName("The cooler Lagoon");
